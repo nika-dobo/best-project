3bd8a593cd6ff407f7cd6e37fab4f3440edd76e0
nika-dobo <nikadobo20@gmail.com>
2025-06-30 16:35:02 +0400

    a

--- web/codewars/app.js
+++ web/codewars/app.js
@@ -1,6 +1,3 @@
-import { neon } from "@netlify/neon";
-const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
-const [post] = await sql`SELECT * FROM posts WHERE id = ${postId}`;
 
 const express = require("express");
 const cors = require("cors");
